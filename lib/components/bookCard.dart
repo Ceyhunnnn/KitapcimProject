eeb126383c81fafe9f72fde285dde8fdd5710e58
Ceyhun Gül <ceyhungul@Ceyhun-Air.kbb.local>
2022-09-16 17:36:08 +0300

    Padding added for book card component

--- lib/components/bookCard.dart
+++ lib/components/bookCard.dart
@@ -40,6 +40,7 @@ class _buildBooksCardsState extends State<buildBooksCards> {
                   itemCount: snapshot.data!.docs.length,
                   itemBuilder: (context, index) {
                     DocumentSnapshot myBooks = snapshot.data!.docs[index];
+                    print(snapshot.data);
                     String photoUrl = '${myBooks['bookUrl']}';
                     return Padding(
                       padding: const EdgeInsets.all(8.0),
@@ -49,130 +50,135 @@ class _buildBooksCardsState extends State<buildBooksCards> {
                           side: BorderSide(color: Colors.grey.shade200),
                           borderRadius: BorderRadius.circular(20.0),
                         ),
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Expanded(
-                                    child: Padding(
-                                  padding: const EdgeInsets.all(10.0),
-                                  child: Text(
-                                    '${myBooks['bookAbout']}',
-                                    style: context.buildTextStyle(
-                                        15, Colors.black),
-                                  ),
-                                ))
-                              ],
-                            ), //kitaba dair aciklama
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Container(
-                                    child: Image.network(photoUrl),
-                                    width: context.dynamicWidth(0.20),
-                                    height: context.dynamicHeight(0.15),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        '${myBooks['bookAuthor']}',
-                                        style: TextStyle(
-                                            fontStyle: FontStyle.italic),
-                                      ),
-                                      SizedBox(
-                                        height: context.dynamicHeight(0.02),
-                                      ),
-                                      Text(
-                                        '${myBooks['bookName']}',
-                                        style: TextStyle(
-                                            fontStyle: FontStyle.italic),
-                                      ),
-                                      SizedBox(
-                                        height: context.dynamicHeight(0.02),
-                                      ),
-                                      Text(
-                                        '${myBooks['bookPageNumber']}',
-                                        style: TextStyle(
-                                            fontStyle: FontStyle.italic),
-                                      ),
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ), //kitap bilgileri
-                            Divider(
-                              indent: 10,
-                              endIndent: 10,
-                              thickness: 2,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Row(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Row(
                                 children: [
-                                  GestureDetector(
-                                    child:
-                                        Icon(Icons.favorite, color: Colors.red),
-                                  ),
-                                  SizedBox(
-                                    width: context.dynamicWidth(0.03),
-                                  ),
-                                  GestureDetector(
-                                    onTap: () {
-                                      setState(() {
-                                        name = '${myBooks['bookName']}';
-                                        author = '${myBooks['bookAuthor']}';
-                                        number = '${myBooks['bookPageNumber']}';
-                                        url = '${myBooks['bookUrl']}';
-                                        about = '${myBooks['bookAbout']}';
-                                      });
-                                      Navigator.push(
-                                        context,
-                                        CupertinoPageRoute(
-                                            builder: (context) =>
-                                                CommentPageDetail(
-                                                    name: name,
-                                                    url: url,
-                                                    author: author,
-                                                    number: number,
-                                                    about: about)),
-                                      );
-                                      print("Girilen Sayfa Index : " +
-                                          index.toString());
-                                    },
-                                    child: Icon(
-                                      Icons.comment_bank_outlined,
+                                  Expanded(
+                                      child: Padding(
+                                    padding: const EdgeInsets.all(10.0),
+                                    child: Text(
+                                      '${myBooks['bookAbout']}',
+                                      style: context.buildTextStyle(
+                                          15, Colors.black),
+                                    ),
+                                  ))
+                                ],
+                              ), //kitaba dair aciklama
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Container(
+                                      child: Image.network(photoUrl),
+                                      width: context.dynamicWidth(0.20),
+                                      height: context.dynamicHeight(0.15),
                                     ),
                                   ),
-                                  SizedBox(
-                                    width: context.dynamicWidth(0.05),
-                                  ),
-                                  Icon(
-                                    Icons.ios_share,
-                                  ),
-                                  Expanded(
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
                                       children: [
-                                        Icon(Icons.save),
+                                        Text(
+                                          '${myBooks['bookAuthor']}',
+                                          style: TextStyle(
+                                              fontStyle: FontStyle.italic),
+                                        ),
                                         SizedBox(
-                                          width: context.dynamicWidth(0.03),
+                                          height: context.dynamicHeight(0.02),
+                                        ),
+                                        Text(
+                                          '${myBooks['bookName']}',
+                                          style: TextStyle(
+                                              fontStyle: FontStyle.italic),
+                                        ),
+                                        SizedBox(
+                                          height: context.dynamicHeight(0.02),
+                                        ),
+                                        Text(
+                                          '${myBooks['bookPageNumber']}',
+                                          style: TextStyle(
+                                              fontStyle: FontStyle.italic),
                                         ),
                                       ],
                                     ),
                                   )
                                 ],
+                              ), //kitap bilgileri
+                              Divider(
+                                indent: 10,
+                                endIndent: 10,
+                                thickness: 2,
                               ),
-                            )
-                          ],
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Row(
+                                  children: [
+                                    GestureDetector(
+                                      child: Icon(Icons.favorite,
+                                          color: Colors.red),
+                                    ),
+                                    SizedBox(
+                                      width: context.dynamicWidth(0.03),
+                                    ),
+                                    GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          name = '${myBooks['bookName']}';
+                                          author = '${myBooks['bookAuthor']}';
+                                          number =
+                                              '${myBooks['bookPageNumber']}';
+                                          url = '${myBooks['bookUrl']}';
+                                          about = '${myBooks['bookAbout']}';
+                                        });
+                                        Navigator.push(
+                                          context,
+                                          CupertinoPageRoute(
+                                              builder: (context) =>
+                                                  CommentPageDetail(
+                                                      name: name,
+                                                      url: url,
+                                                      author: author,
+                                                      number: number,
+                                                      about: about)),
+                                        );
+                                        print("Girilen Sayfa Index : " +
+                                            index.toString());
+                                      },
+                                      child: Icon(
+                                        Icons.comment_bank_outlined,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: context.dynamicWidth(0.05),
+                                    ),
+                                    Icon(
+                                      Icons.ios_share,
+                                    ),
+                                    Expanded(
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
+                                        children: [
+                                          Icon(Icons.save),
+                                          SizedBox(
+                                            width: context.dynamicWidth(0.03),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     );
