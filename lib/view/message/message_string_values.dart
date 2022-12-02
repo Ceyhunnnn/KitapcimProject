44799fa2c15ce5fdbca7717e188c6ceb9d575784
Ceyhun-gl <ceyhungul148>
2022-12-02 12:01:47 +0300

    message view and string values created

---
+++ lib/view/message/message_string_values.dart
@@ -0,0 +1,7 @@
+part of 'message_view.dart';
+
+class _MessasgeStringValues {
+  static _MessasgeStringValues instance = _MessasgeStringValues._init();
+  _MessasgeStringValues._init();
+  final appBarTitle = "Mesajlar";
+}
---
+++ lib/view/message/message_view.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:kitapcim/core/extensions/context_extentions.dart';
+part 'message_string_values.dart';
+
+class Message extends StatefulWidget {
+  const Message({Key? key}) : super(key: key);
+
+  @override
+  State<Message> createState() => _MessageState();
+}
+
+class _MessageState extends State<Message> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: context.appColor,
+        centerTitle: true,
+        title: Text(_MessasgeStringValues.instance.appBarTitle),
+      ),
+      body: Column(),
+    );
+  }
+}
