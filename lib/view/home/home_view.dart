74dfc5359fb8fda1543f28cd379afd159fdd6ab5
Ceyhun-gl <ceyhungul148>
2022-12-07 18:48:10 +0300

    app updated for setting

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,7 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 import 'package:kitapcim/core/extensions/context_extentions.dart';
 
--- lib/view/message/message_view.dart
+++ lib/view/message/message_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
 import 'package:kitapcim/core/extensions/context_extentions.dart';
 part 'message_string_values.dart';
 
